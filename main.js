d642af35f5cef5aeca57a4b97c22994a32a564aa
Master Vincent <vincent.samuelsson@hotmail.com>
2024-01-29 11:26:41 +0100

    added.json

--- main.js
+++ main.js
@@ -8,16 +8,35 @@
 // console.log(3);
 // console.log(4);
 
-const request = new XMLHttpRequest();
+const getToDos = (callback) => {
+  const request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange", () => {
-  //   console.log(request, request.readyState);
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request, request.responseText);
-  } else if (request.readyState === 4) {
-    console.log("could not festh data");
+  request.addEventListener("readystatechange", () => {
+    //   console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
+    } else if (request.readyState === 4) {
+      callback("could not fetch data", undefined);
+    }
+  });
+
+  request.open("GET", "todos.json");
+  request.send();
+};
+
+console.log(1);
+console.log(2);
+
+getToDos((err, data) => {
+  console.log("callback fired");
+  console.log(err, data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
 
-request.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
-request.send();
+console.log(3);
+console.log(4);
