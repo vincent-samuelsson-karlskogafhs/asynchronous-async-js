0153bf5ed3cd8e2e6186951d879d13d55f54465a
Master Vincent <vincent.samuelsson@hotmail.com>
2024-01-30 11:53:32 +0100

    added fetch code

--- main.js
+++ main.js
@@ -1,74 +1,74 @@
-// console.log(1);
-// console.log(2);
+// // console.log(1);
+// // console.log(2);
 
-// setTimeout(() => {
-//   console.log("call back fired");
-// }, 2000);
+// // setTimeout(() => {
+// //   console.log("call back fired");
+// // }, 2000);
 
-// console.log(3);
-// console.log(4);
+// // console.log(3);
+// // console.log(4);
 
-const getToDos = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+// const getToDos = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
 
-    request.addEventListener("readystatechange", () => {
-      //   console.log(request, request.readyState);
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject("error getting resource");
-      }
-    });
-
-    request.open("GET", resource);
-    request.send();
-  });
-};
-
-getToDos("todos/luigi.json")
-  .then((data) => {
-    console.log("promise 1 resolved:", data);
-    return getToDos("todos/mario.json");
-  })
-  .then((data) => {
-    console.log("promise 2 resolved", data);
-    return getToDos("todos/shaun.json");
-  })
-  .then((data) => {
-    console.log("promise 3 resolved");
-  })
-  .catch((err) => {
-    console.log("promise rejected:", err);
-  });
-
-// skicka ny promise request - temp
-
-const getSomething = () => {
-  return new Promise((resolve, reject) => {
-    // fetch something - document - resolve - sucess or error - parrameter - build in promisses - in JS
-    resolve("some data");
-    // reject("some error");
-  });
-};
-
-// getToDos("todos/luigi.json", (err, data) => {
-//   console.log(data);
-//   getToDos("todos/mario.json", (err, data) => {
-//     console.log(data);
-//     getToDos("todos/shaun.json", (err, data) => {
-//       console.log(data);
+//     request.addEventListener("readystatechange", () => {
+//       //   console.log(request, request.readyState);
+//       if (request.readyState === 4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText);
+//         resolve(data);
+//       } else if (request.readyState === 4) {
+//         reject("error getting resource");
+//       }
 //     });
-//   });
-// });
 
-// hämta data med function "data"
-// getSomething()
+//     request.open("GET", resource);
+//     request.send();
+//   });
+// };
+
+// getToDos("todos/luigi.json")
 //   .then((data) => {
-//     console.log(data);
+//     console.log("promise 1 resolved:", data);
+//     return getToDos("todos/mario.json");
 //   })
-// function - get data, logg saved data (err)
-// .catch((err) => {
-//   console.log(err);
-// });
+//   .then((data) => {
+//     console.log("promise 2 resolved", data);
+//     return getToDos("todos/shaun.json");
+//   })
+//   .then((data) => {
+//     console.log("promise 3 resolved");
+//   })
+//   .catch((err) => {
+//     console.log("promise rejected:", err);
+//   });
+
+// // skicka ny promise request - temp
+
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     // fetch something - document - resolve - sucess or error - parrameter - build in promisses - in JS
+//     resolve("some data");
+//     // reject("some error");
+//   });
+// };
+
+// // getToDos("todos/luigi.json", (err, data) => {
+// //   console.log(data);
+// //   getToDos("todos/mario.json", (err, data) => {
+// //     console.log(data);
+// //     getToDos("todos/shaun.json", (err, data) => {
+// //       console.log(data);
+// //     });
+// //   });
+// // });
+
+// // hämta data med function "data"
+// // getSomething()
+// //   .then((data) => {
+// //     console.log(data);
+// //   })
+// // function - get data, logg saved data (err)
+// // .catch((err) => {
+// //   console.log(err);
+// // });
--- sandboxapi.js
+++ sandboxapi.js
@@ -1,11 +1,25 @@
-fetch("todos/luigi.json")
-  .then((response) => {
-    console.log("resolved", response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("rejected", err);
-  });
+// assync ger alltid promise som retur
+const getToDos = async () => {
+  const repsonse = await fetch("todos/luigi.json");
+  const data = await repsonse.json();
+  return data;
+};
+
+console.log(1);
+console.log(2);
+getToDos().then((data) => console.log("resolved:", data));
+
+console.log(3);
+console.log(4);
+
+// fetch("todos/luigi.json")
+//   .then((response) => {
+//     console.log("resolved", response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("rejected", err);
+//   });
