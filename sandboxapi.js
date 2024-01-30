14ba3e10808ee2570d696d6d3ce9a8b49af919d5
Master Vincent <vincent.samuelsson@hotmail.com>
2024-01-30 11:33:49 +0100

    added fetsh code

---
+++ sandboxapi.js
@@ -0,0 +1,11 @@
+fetch("todos/luigi.json")
+  .then((response) => {
+    console.log("resolved", response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log("rejected", err);
+  });
